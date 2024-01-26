b7b6e9372c7b1b2b8cd7e179319bd8a0333aff9a
vlad546789 <157151454+vlad546789@users.noreply.github.com>
2024-01-26 21:02:11 +0300

    Add files via upload

---
+++ MoveCube.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    //Здоровье NCP
+    public int health = 5;
+
+    //Уровень NCP
+    public int level = 1;
+
+    //Скорость NCP
+    public float speed = 1.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
